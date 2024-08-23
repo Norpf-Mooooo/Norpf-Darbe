e36d65b6f9498e614fec2e5838a06e8fa1b00964
Norpf <133520141+Norpf-Mooooo@users.noreply.github.com>
2024-08-23 15:50:22 +0300

    Add files via upload

--- npf-darbe/client.lua
+++ npf-darbe/client.lua
@@ -1,34 +1,25 @@
--- İzin verilen Steam adını belirtin
 local allowedSteamName = "Stravechi" -- Buraya kendi Steam adınızı ekleyin
 
--- /darbe komutunu kaydet
 RegisterCommand("darbe", function()
     local playerId = PlayerId()
-    -- Sunucuya oyuncunun Steam adını gönder
     TriggerServerEvent("checkSteamName", allowedSteamName)
 end)
 
--- NUI'nin kapanmasıyla ilgili olay
 RegisterNUICallback("close", function(data, cb)
     SetNuiFocus(false, false)
-    -- Video bitiminde sunucudan oyuncuları atacak komut
     TriggerServerEvent("server:kickAllPlayers")
     cb("ok")
 end)
 
--- Sunucudan yanıt al
 RegisterNetEvent("receiveSteamNameCheck")
 AddEventHandler("receiveSteamNameCheck", function(isAllowed)
     if isAllowed then
-        -- NUI'yi aç ve video oynamasını başlat
         SendNUIMessage({
             type = "open",
-            video = "darbe.mp4" -- Oynatmak istediğiniz video dosyasının adı
+            video = "darbe.mp4"
         })
-        -- NUI odağını aç
         SetNuiFocus(true, true)
     else
-        -- Oyuncunun izinli olup olmadığını kontrol etme mesajı
         TriggerEvent('chat:addMessage', { args = { '^1Error', 'Bu komutu kullanma izniniz yok!' } })
     end
 end)
--- npf-darbe/server.lua
+++ npf-darbe/server.lua
@@ -4,19 +4,15 @@ AddEventHandler("checkSteamName", function(allowedSteamName)
     local identifiers = GetPlayerIdentifiers(source)
     local playerName = GetPlayerName(source)
 
-    -- Steam adını kontrol et
     if playerName == allowedSteamName then
-        -- Eğer Steam adı doğruysa tüm oyunculara video göster
-        TriggerClientEvent("receiveSteamNameCheck", -1, true) -- -1, sunucudaki tüm oyunculara gönderir
+        TriggerClientEvent("receiveSteamNameCheck", -1, true)
     else
-        -- Eğer Steam adı yanlışsa sadece komutu yazan kişiye hata gönder
         TriggerClientEvent("receiveSteamNameCheck", source, false)
     end
 end)
 
 RegisterServerEvent("server:kickAllPlayers")
 AddEventHandler("server:kickAllPlayers", function()
-    -- Tüm oyuncuları at
     for _, playerId in ipairs(GetPlayers()) do
         DropPlayer(playerId, "SCRP İyi Günler Diler.")
     end
